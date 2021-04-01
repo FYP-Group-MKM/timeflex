0a9a9306bab3f61f7c6311a4c59d0812f0c47be9
darrenleung1208hk <leungyatlong1208@gmail.com>
2021-04-01 18:11:07 +0800

    update

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,6 @@ router.get('/logout', (req, res) => {
     res.redirect('/');
 });
 
-
 // auth with google+
 router.get('/google',
     (req, res, next) => {
@@ -16,7 +15,6 @@ router.get('/google',
     },
     passport.authenticate('google', { scope: ['profile'] })
 );
-
 
 // callback route for google to redirect to
 // hand control to passport to use code to grab profile info
