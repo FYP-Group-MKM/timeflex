dc452184f9cefc40e6ad9cd28fcb37a146379e40
darrenleung1208hk <leungyatlong1208@gmail.com>
2021-04-02 23:49:22 +0800

    update

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,16 +9,12 @@ router.get('/login/success', (req, res) => {
     }
 });
 
-// auth with google
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
-// callback route for google to redirect to
-// hand control to passport to use code to grab profile info
 router.get('/google/redirect', passport.authenticate('google', {
     successRedirect: "http://localhost:3000/calendar"
 }));
 
-// auth logout
 router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('http://localhost:3000');
