b3215c3edfb63b3a7b5e21b10ca2212ef7c3a0be
darrenleung1208hk <leungyatlong1208@gmail.com>
2021-04-06 23:58:33 +0800

    keys

---
+++ server/config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mongodb: {
+        dbURI: 'mongodb+srv://kolpl:kolpl1997@memoryproject.vss6e.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    },
+    google: {
+        clientID: '211712247388-ej3q6imgf8l8pj0nil5ji8i9fb1rshqn.apps.googleusercontent.com',
+        clientSecret: 'oLai4Sj4tRUfLHQQdocdU_TT',
+    },
+    session: {
+        cookieKey: 'timflex-by-group-mkm'
+    }
+};
